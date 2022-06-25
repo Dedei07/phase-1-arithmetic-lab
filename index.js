c517651ee067d9681cb022540d115d227cc95547
valentinadinnadedeiokai <valentinaokai@gmail.com>
2022-06-25 13:16:32 -0700

    finished assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+   let multiply = (num1*num2)
+   let num1 = 2
+   let num2 = 31
+  multiply = 2*31
+  multiply;
+  //=> 62
 
-// Write your code here
+  let random = (Number > 0)
+  Math.floor (2) + 0
+    random = 2
+  //=> true
+
+  let mod =(num3/num4)
+    let num3 = 16
+    let num4 = 4
+    mod = 16/4
+    mod;
+    //=> 4
+
+    let max = (Number = 20)
+    Math.max (5,10,15,20)
+    max;
+    //=> 20
